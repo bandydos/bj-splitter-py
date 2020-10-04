df2d2602dcd5d74b187a366a3c657e7e9c01be70
KR44I <38465150+AlexaTack@users.noreply.github.com>
2020-10-04 22:40:47 +0200

    Game logic update 2

--- cards-dealer/cards-dealer-py.py
+++ cards-dealer/cards-dealer-py.py
@@ -50,20 +50,22 @@ class Player(object):
         self.name = name
         self.hand = []
         self.score = 0
+        self.status = ""
 
     def draw(self, deck):
         self.hand.append(deck.draw())
         return self  # For chaining.
 
     def calcscore(self):
+        self.score = 0  # Set score back to 0.
         for c in self.hand:
-            self.score += c.value
+            self.score += c.value  # Calc total.
 
     def show(self):
         print(self.name)
         for c in self.hand:
             c.show()
-        print(self.score)
+        print(f"Score: {self.score}, status: {self.status}")
         print("\n")
 
 
@@ -76,24 +78,50 @@ class Table(object):
         self.build()
 
     def build(self):
-        self.players.append(self.dealer)
+        # self.players.append(self.dealer)
         for i in range(3):
-            self.players.append(Player(f"Player{i + 1}"))
+            self.players.append(Player(f"Player {i + 1}"))
 
     def play(self):
-        self.deck.shuffle()
+        self.deck.shuffle()  # Shuffle the deck.
+
+        # Dealer.
+        for _ in range(2):
+            self.dealer.draw(self.deck)  # Dealer draws 2 cards.
+        self.dealer.calcscore()  # Calculate score.
+
+        while(self.dealer.score < 17):  # Dealer has to stop at 17 or higher.
+            self.dealer.draw(self.deck)
+            self.dealer.calcscore()
+        # Players.
         for p in self.players:
             for _ in range(2):
-                p.draw(self.deck)
-            p.calcscore()
+                p.draw(self.deck)  # Players draw 2 cards.
+            p.calcscore()  # Calculate score.
+
+            # Calculate when to draw another card.
+            if((self.dealer.hand[0].value > 7 and self.dealer.hand[0].value > p.score - 10) or p.score < 12):
+                while((p.score < self.dealer.hand[0].value + 10) or p.score < 17): # Something goes wrong here!
+                    p.draw(self.deck)
+                    p.calcscore()
+
+            if(p.score == 21 and len(p.hand) < 3):
+                p.status == "blackjack"
+            if((p.score < 22 and self.dealer.score > 21) or (p.score > self.dealer.score and p.score < 22)):
+                p.status = "win"
+            elif(p.score > 21):
+                p.status = "bust"
+            elif(p.score == self.dealer.score):
+                p.status = "push"
+            else:
+                p.status = "lost"
 
     def show(self):
+        self.dealer.show()
         for p in self.players:
             p.show()
 
 
 t1 = Table("T1")
-print(str((len(t1.deck.cards))))
 t1.play()
 t1.show()
-print(str((len(t1.deck.cards))))
